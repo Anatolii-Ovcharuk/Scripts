ba39131a9f49d841c0203dfaa3ad458dd4148d19
Anatolii <144040851+Anatolii-Ovcharuk@users.noreply.github.com>
2024-08-10 17:41:48 +0200

    Add scripts and update

--- PowerShell-Script's/Injection_restore_system_for_Windows_v.1.0-auto.ps1
+++ PowerShell-Script's/Injection_restore_system_for_Windows_v.1.0-auto.ps1
@@ -1,42 +1,46 @@
 # Injection: Restore system for Windows. Made by Anatolii Ovcharuk. Version 1.0 for Windows - Update 10.08.2024 / MIT License
 # Profile on Github: https://github.com/Anatolii-Ovcharuk
 # This is a PowerShell script for Windows, using PowerShell Console or PowerShell ISE Console. Approximate completion time: 30 minutes.
-# Notice for future: This script is automated. The system will restart.
+# Notice for future: This script is automated. With restart system.
 
 # If not working, set in command: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 
-# Информация в консоли / Information in console
+# Options / Опции
 Write-Host "Initializating..."
+$delayInSeconds = 3 # Значение задержки в переменной (в секундах) / Delay for running command's (in seconds)
+$timeToRestart = 60 # Пауза перед началом рестарта системы (в секундах) / Pause before starting restart system (in seconds)
+
+# Информация в консоли / Information in console
 Write-Host "Injection: Restore system for Windows. Made by Anatolii Ovcharuk. Version 1.0 for Windows - Update 10.08.2024 | MIT License" -ForegroundColor Green -BackgroundColor Black
 Write-Host "Profile on Github: https://github.com/Anatolii-Ovcharuk" -ForegroundColor Green -BackgroundColor Black
 Write-Host "This is PowerShell script for Windows, using PowerShell Console or PowerShell ISE Console. Approximate completion time: 30 minutes." -ForegroundColor Yellow -BackgroundColor Black
-Write-Host "Notice for future: This script is automated. The system will restart." -ForegroundColor Red -BackgroundColor Black
+Write-Host "Notice for future: This script is automated. With restart system." -ForegroundColor Red -BackgroundColor Black
 
 Write-Host "Starting process..." -ForegroundColor Green
 
 # Операции в Windows Powershell Console / Operation in Windows Powershell Console
 
 # Запуск DISM command / Running DISM command
-Write-Host "Waiting for 10 seconds before running DISM command..." -ForegroundColor Yellow
-Start-Sleep -Seconds 10
+Write-Host "Waiting for $delayInSeconds seconds before running DISM command..." -ForegroundColor Yellow
+Start-Sleep -Seconds $delayInSeconds
 Write-Host "Running DISM command..." -ForegroundColor Red
 DISM.exe /Online /Cleanup-image /Restorehealth
 
 # Запуск SFC command / Running SFC command
-Write-Host "Waiting for 10 seconds before running SFC command..." -ForegroundColor Yellow
-Start-Sleep -Seconds 10
+Write-Host "Waiting for $delayInSeconds seconds before running SFC command..." -ForegroundColor Yellow
+Start-Sleep -Seconds $delayInSeconds
 Write-Host "Running SFC command..." -ForegroundColor Red
 SFC.exe /SCANNOW
 
 # Запуск Verifier / Running Verifier
-Write-Host "Waiting for 10 seconds before running Verifier..." -ForegroundColor Yellow
-Start-Sleep -Seconds 10
+Write-Host "Waiting for $delayInSeconds seconds before running Verifier..." -ForegroundColor Yellow
+Start-Sleep -Seconds $delayInSeconds
 Write-Host "Running Verifier..." -ForegroundColor Red
 verifier.exe /query
 
 # Запуск Memory Diagnostic / Running Memory Diagnostic
-Write-Host "Waiting for 10 seconds before running Memory Diagnostic..." -ForegroundColor Yellow
-Start-Sleep -Seconds 10
+Write-Host "Waiting for $delayInSeconds seconds before running Memory Diagnostic..." -ForegroundColor Yellow
+Start-Sleep -Seconds $delayInSeconds
 Write-Host "Running Memory Diagnostic..." -ForegroundColor Red
 Write-Host "Для продолжения работы необходимо будет перезагрузить систему и выполнить проверку памяти." -ForegroundColor Red
 mdsched.exe
@@ -48,15 +52,24 @@ $wshell.SendKeys("{DOWN}")
 Start-Sleep -Seconds 2
 $wshell.SendKeys("{ENTER}")
 
+# Результат / Result
+Write-Host "Injection: Restore system for Windows - Complete." -ForegroundColor Green
+Start-Sleep -Seconds $delayInSeconds
+
 # Перезагрузка Windows / Restart Windows
-Write-Host "System will restart in 60 seconds..." -ForegroundColor Red
+Write-Host "Warning! Restart system and exit..." -ForegroundColor Yellow
+Start-Sleep -Seconds $delayInSeconds
+# Write-Host "System will restart in 60 seconds..." -ForegroundColor Red
+Write-Host "System will restart in $timeToRestart seconds..." -ForegroundColor Red
 Start-Sleep -Seconds 3
-shutdown.exe -r -t 60
+# shutdown.exe -r -t 60
+shutdown.exe -r -t $timeToRestart
 Write-Host "Exit..."
-Start-Sleep -Seconds 10
+Start-Sleep -Seconds $delayInSeconds
 exit
 
-
+# =========== =========== ===========  Ending script =========== =========== =========== 
+# Thank's for using my script's... With respect's, Anatolii Ovcharuk.
 
 
 
@@ -127,3 +140,5 @@ exit
 # Write-Host "Exit..."
 # Start-Sleep -Seconds 10
 # exit
+
+
---
+++ PowerShell-Script's/Upgrade_operation_system_windows_server_2016-2019_v.1.1-auto.ps1
@@ -0,0 +1,159 @@
+# Upgrade operation system Windows server 2016 - 2019. Made by Anatolii Ovcharuk. Version 1.1 for Windows - Update 10.08.2024 / MIT License
+# Profile on Github: https://github.com/Anatolii-Ovcharuk
+# This is PowerShell file script for Windows, using PowerShell Console or PowerShell ISE Console. Approximate completion time: 15 minutes.
+# Upgrade including: Add Windows Photo Viewer; Add monitoring disk in manager; Add WLAN services.
+# Notice for future: This script is automated. With restart system.
+
+# If not working, set in command: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+
+# Options / Опции
+Write-Host "Initializating..."
+$delayInSeconds = 3 # Значение задержки в переменной (в секундах) / Delay for running command's (in seconds)
+$timeToRestart = 60 # Пауза перед началом рестарта системы (в секундах) / Pause before starting restart system (in seconds)
+
+# Информация в консоли / Information in console
+Write-Host "Upgrade operation system Windows server 2016 - 2019. Made by Anatolii Ovcharuk. Version 1.1 for Windows - Update 10.08.2024 / MIT License" -ForegroundColor Green -BackgroundColor Black
+Write-Host "Profile on Github: https://github.com/Anatolii-Ovcharuk" -ForegroundColor Green -BackgroundColor Black
+Write-Host "This is PowerShell file script for Windows, using PowerShell Console or PowerShell ISE Console. Approximate completion time: 15 minutes." -ForegroundColor Yellow -BackgroundColor Black
+Write-Host "Upgrade including: Add Windows Photo Viewer; Add monitoring disk in manager; Add WLAN services." -ForegroundColor Yellow -BackgroundColor Black
+Write-Host "Notice for future: This script is automated. With restart system." -ForegroundColor Red -BackgroundColor Black
+
+Start-Sleep -Seconds $delayInSeconds
+Write-Host "Starting process..." -ForegroundColor Green
+Start-Sleep -Seconds $delayInSeconds
+
+# Операции в Windows Powershell Console / Operation in Windows Powershell Console
+
+# Operation in Windows Powershell - Add Windows Photo Viewer;
+Write-Host "Waiting for $delayInSeconds seconds before running - Add Windows Photo Viewer..." -ForegroundColor Red 
+Start-Sleep -Seconds $delayInSeconds
+regsvr32 "C:\Program Files (x86)\Windows Photo Viewer\PhotoViewer.dll"
+If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll" -ItemType RegistryKey -Force }
+If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell" -ItemType RegistryKey -Force }
+If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open" -ItemType RegistryKey -Force }
+Set-ItemProperty -path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open" -Name "MuiVerb" -Type "String" -Value "@photoviewer.dll,-3043"
+If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\command")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\command" -ItemType RegistryKey -Force }
+Set-ItemProperty -path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\command" -Name "(Default)" -Type "ExpandString" -Value "%SystemRoot%\System32\rundll32.exe `"%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll`", ImageView_Fullscreen %1"
+If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\DropTarget")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\DropTarget" -ItemType RegistryKey -Force }
+Set-ItemProperty -path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\DropTarget" -Name "Clsid" -Type "String" -Value "{FFE2A43C-56B9-4bf5-9A79-CC6D4285608A}"
+If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print" -ItemType RegistryKey -Force }
+If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\command")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\command" -ItemType RegistryKey -Force }
+Set-ItemProperty -path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\command" -Name "(Default)" -Type "ExpandString" -Value "%SystemRoot%\System32\rundll32.exe `"%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll`", ImageView_Fullscreen %1"
+If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\DropTarget")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\DropTarget" -ItemType RegistryKey -Force }
+Set-ItemProperty -path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\DropTarget" -Name "Clsid" -Type "String" -Value "{60fd46de-f830-4894-a628-6fa81bc0190d}"
+
+# Operation in Windows Powershell - Add monitoring disk in manager;
+Write-Host "Waiting for $delayInSeconds seconds before running - Add monitoring disk in manager..." -ForegroundColor Red 
+Start-Sleep -Seconds $delayInSeconds
+diskperf -y
+
+# Operation in Windows Powershell - Add WLAN services;
+Write-Host "Waiting for $delayInSeconds seconds before running - Add WLAN services..." -ForegroundColor Red 
+Start-Sleep -Seconds $delayInSeconds
+Get-WindowsFeature *Wireless*
+Install-WindowsFeature -Name Wireless-Networking
+Set-Service WlanSvc –startuptype automatic –passthru
+Start-Service WlanSvc –PassThru 
+
+# Результат / Result
+Write-Host "Upgrade operation system Windows - Complete." -ForegroundColor Green
+Start-Sleep -Seconds $delayInSeconds
+
+# Перезагрузка Windows / Restart Windows
+Write-Host "Warning! Restart system and exit..." -ForegroundColor Yellow
+Start-Sleep -Seconds $delayInSeconds
+# Write-Host "System will restart in 60 seconds..." -ForegroundColor Red
+Write-Host "System will restart in $timeToRestart seconds..." -ForegroundColor Red
+Start-Sleep -Seconds 1
+# shutdown.exe -r -t 60
+shutdown.exe -r -t $timeToRestart
+Write-Host "Exit..."
+Start-Sleep -Seconds $delayInSeconds
+exit
+
+
+# =========== =========== ===========  Ending script =========== =========== =========== 
+# Thank's for using my script's... With respect's, Anatolii Ovcharuk.
+
+
+
+
+
+
+
+
+
+
+# # Upgrade operation system Windows server 2016 - 2019. Made by Anatolii Ovcharuk. Version 1.1 for Windows - Update 10.08.2024 / MIT License
+# # Profile on Github: https://github.com/Anatolii-Ovcharuk
+# # This is PowerShell file script for Windows, using PowerShell Console or PowerShell ISE Console. Approximate completion time: 15 minutes.
+# # Upgrade including: Add Windows Photo Viewer; Add monitoring disk in manager; Add WLAN services.
+# # Notice for future: This script is automated. With restart system.
+
+# # If not working, set in command: Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+
+# # Options / Опции
+# Write-Host "Initializating..."
+# $delayInSeconds = 3 # Значение задержки в переменной (в секундах) / Delay for running command's (in seconds)
+# $timeToRestart = 60 # Пауза перед началом рестарта системы (в секундах) / Pause before starting restart system (in seconds)
+
+# # Информация в консоли / Information in console
+# Write-Host "Upgrade operation system Windows server 2016 - 2019. Made by Anatolii Ovcharuk. Version 1.1 for Windows - Update 10.08.2024 / MIT License" -ForegroundColor Green -BackgroundColor Black
+# Write-Host "Profile on Github: https://github.com/Anatolii-Ovcharuk" -ForegroundColor Green -BackgroundColor Black
+# Write-Host "This is PowerShell file script for Windows, using PowerShell Console or PowerShell ISE Console. Approximate completion time: 15 minutes." -ForegroundColor Yellow -BackgroundColor Black
+# Write-Host "Upgrade including: Add Windows Photo Viewer; Add monitoring disk in manager; Add WLAN services." -ForegroundColor Yellow -BackgroundColor Black
+# Write-Host "Notice for future: This script is automated. With restart system." -ForegroundColor Red -BackgroundColor Black
+
+# Start-Sleep -Seconds $delayInSeconds
+# Write-Host "Starting process..." -ForegroundColor Green
+# Start-Sleep -Seconds $delayInSeconds
+
+# # Операции в Windows Powershell Console / Operation in Windows Powershell Console
+
+# # Operation in Windows Powershell - Add Windows Photo Viewer;
+# Write-Host "Waiting for $delayInSeconds seconds before running - Add Windows Photo Viewer..." -ForegroundColor Red 
+# Start-Sleep -Seconds $delayInSeconds
+# regsvr32 "C:\Program Files (x86)\Windows Photo Viewer\PhotoViewer.dll"
+# If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll" -ItemType RegistryKey -Force }
+# If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell" -ItemType RegistryKey -Force }
+# If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open" -ItemType RegistryKey -Force }
+# Set-ItemProperty -path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open" -Name "MuiVerb" -Type "String" -Value "@photoviewer.dll,-3043"
+# If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\command")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\command" -ItemType RegistryKey -Force }
+# Set-ItemProperty -path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\command" -Name "(Default)" -Type "ExpandString" -Value "%SystemRoot%\System32\rundll32.exe `"%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll`", ImageView_Fullscreen %1"
+# If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\DropTarget")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\DropTarget" -ItemType RegistryKey -Force }
+# Set-ItemProperty -path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\open\DropTarget" -Name "Clsid" -Type "String" -Value "{FFE2A43C-56B9-4bf5-9A79-CC6D4285608A}"
+# If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print" -ItemType RegistryKey -Force }
+# If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\command")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\command" -ItemType RegistryKey -Force }
+# Set-ItemProperty -path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\command" -Name "(Default)" -Type "ExpandString" -Value "%SystemRoot%\System32\rundll32.exe `"%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll`", ImageView_Fullscreen %1"
+# If ( -Not ( Test-Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\DropTarget")) { New-Item -Path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\DropTarget" -ItemType RegistryKey -Force }
+# Set-ItemProperty -path "Registry::HKEY_CLASSES_ROOT\Applications\photoviewer.dll\shell\print\DropTarget" -Name "Clsid" -Type "String" -Value "{60fd46de-f830-4894-a628-6fa81bc0190d}"
+
+# # Operation in Windows Powershell - Add monitoring disk in manager;
+# Write-Host "Waiting for $delayInSeconds seconds before running - Add monitoring disk in manager..." -ForegroundColor Red 
+# Start-Sleep -Seconds $delayInSeconds
+# diskperf -y
+
+# # Operation in Windows Powershell - Add WLAN services;
+# Write-Host "Waiting for $delayInSeconds seconds before running - Add WLAN services..." -ForegroundColor Red 
+# Start-Sleep -Seconds $delayInSeconds
+# Get-WindowsFeature *Wireless*
+# Install-WindowsFeature -Name Wireless-Networking
+# Set-Service WlanSvc –startuptype automatic –passthru
+# Start-Service WlanSvc –PassThru 
+
+# # Результат / Result
+# Write-Host "Upgrade operation system Windows - Complete." -ForegroundColor Green
+# Start-Sleep -Seconds $delayInSeconds
+
+# # Перезагрузка Windows / Restart Windows
+# Write-Host "Warning! Restart system and exit..." -ForegroundColor Yellow
+# Start-Sleep -Seconds $delayInSeconds
+# Write-Host "System will restart in $timeToRestart seconds..." -ForegroundColor Red
+# Start-Sleep -Seconds 1
+# shutdown.exe -r -t $timeToRestart
+# Write-Host "Exit..."
+# Start-Sleep -Seconds $delayInSeconds
+# exit
+
+# # =========== =========== ===========  Ending script =========== =========== =========== 
+# # Thank's for using my script's... With respect's, Anatolii Ovcharuk
