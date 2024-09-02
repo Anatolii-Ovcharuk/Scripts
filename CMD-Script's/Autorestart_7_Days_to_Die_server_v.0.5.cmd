81bcdd085eb7394db32aa18561e1e0630cc8dd45
Anatolii <144040851+Anatolii-Ovcharuk@users.noreply.github.com>
2024-09-02 23:46:32 +0200

    Add script

---
+++ CMD-Script's/Autorestart_7_Days_to_Die_server_v.0.5.cmd
@@ -0,0 +1,58 @@
+@echo off
+
+
+REM Autorestart: 7DTD Server for Windows. Made by Anatolii Ovcharuk. Version 0.5 for Windows - Update 01.09.2024 / MIT License
+REM Profile on Github: https://github.com/Anatolii-Ovcharuk
+REM This is .cmd file script for Windows, using CMD Console.
+REM Notice for future: This script automated. This is version for server.
+
+
+echo Autorestart: 7DTD Server for Windows. Made by Anatolii Ovcharuk. Version 0.5 for Windows - Update 01.09.2024 / MIT License
+echo Profile on Github: https://github.com/Anatolii-Ovcharuk
+echo This is .cmd file script for Windows, using CMD Console.
+echo Notice for future: This script automated. This is version for server.
+
+echo Initializating...
+
+setlocal enabledelayedexpansion
+
+:: Установите время между перезапусками в секундах (6 часов = 21600 секунд, 12 часов = 43200 секунд, 24 часа - 86400 секунд)
+set "restart_interval=43200"
+
+:start
+echo Starting 7 Days to Die server...
+
+:: Переход в каталог с сервером
+cd /d "D:\Steam\steamapps\common\7 Days to Die Dedicated Server"
+
+
+:: Запуск сервера
+REM Запускает сервер в обычном режиме.
+REM start /wait 7DaysToDieServer.exe -configfile=serverconfig.xml
+REM Запускает сервер в фоновом режиме.
+start "" /b 7DaysToDieServer.exe -configfile=serverconfig.xml
+
+:: Ждем заданное время
+timeout /t %restart_interval% /nobreak
+
+
+REM This is OLD version to off application...
+REM :: Убиваем процесс сервера
+REM taskkill /f /im 7DaysToDieServer.exe
+
+REM This is NEW version to off application...
+:: Использование PowerShell для корректного завершения процесса сервера
+REM USE ONE OF ALL STRING:
+REM powershell -command "Get-Process 7DaysToDieServer | Stop-Process -Force"
+REM powershell -NoProfile -Command "Get-Process 7DaysToDieServer | Stop-Process -Force; exit"
+REM powershell -NoProfile -Command "Get-Process 7DaysToDieServer | Stop-Process; exit"
+
+REM Использование -ErrorAction SilentlyContinue помогает избежать сообщений об ошибках, если процесс не найден или уже завершен.
+powershell -NoProfile -Command "Get-Process 7DaysToDieServer -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue"
+
+
+:: Небольшая задержка перед перезапуском
+timeout /t 10
+
+:: Перезапуск сервера
+goto start
