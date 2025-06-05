48b589391766cba9c77df048f764d87801bbc2a6
Anatolii <144040851+Anatolii-Ovcharuk@users.noreply.github.com>
2025-06-05 17:28:33 +0200

    Add Startup script for MongoDB

---
+++ BAT-Scripts/Startup_MongoDB/Startup_MongoDB_v.0.1.bat
@@ -0,0 +1,16 @@
+@echo off
+
+
+REM Startup: MongoDB for Windows. Made by Anatolii Ovcharuk. Version 0.1 for Windows - Update 06.06.2024 / MIT License
+REM Profile on Github: https://github.com/Anatolii-Ovcharuk
+REM This is .bat file script for Windows, using CMD Console.
+REM Notice for future: This script automated. This is version for server.
+
+echo Startup: MongoDB for Windows. Made by Anatolii Ovcharuk. Version 0.1 for Windows - Update 06.06.2024 / MIT License
+echo Profile on Github: https://github.com/Anatolii-Ovcharuk
+echo This is .bat file script for Windows, using CMD Console.
+echo Notice for future: This script automated. This is version for server.
+
+echo Initializating - Startup MongoDB...
+net start MongoDB
+pause
